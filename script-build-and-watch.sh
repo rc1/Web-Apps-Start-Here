5cb396a6c14bd4487430af04476bfb0ee6f40df8
Ross Cairns <ross@electricglen.com>
2016-02-29 16:20:55 +0000

    Removed NVM

--- script-build-and-watch.sh
+++ script-build-and-watch.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
-    echo "System: OSX"
-    source $(brew --prefix nvm)/nvm.sh
-    nvm use
-elif [[ $OSTYPE == "linux-gnueabihf" ]]; then
-    # Do something under Mac OS X platform
-    echo "System: Raspberry Pi 2"
-elif [ "$(uname)" == "Linux" ]; then
-    echo "System: Linux"
-fi
-
 gulp && gulp watch
--- script-run.sh
+++ script-run.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
-    echo "System: OSX"
-    source $(brew --prefix nvm)/nvm.sh
-    nvm use
-elif [[ $OSTYPE == "linux-gnueabihf" ]]; then
-    # Do something under Mac OS X platform
-    echo "System: Raspberry Pi 2"
-elif [ "$(uname)" == "Linux" ]; then
-    echo "System: Linux"
-fi
-
 PORT=7080 \
 IS_LOCAL=1 \
-iojs -harmony_arrow_functions  app.server.js
+node -harmony_arrow_functions  app.server.js
